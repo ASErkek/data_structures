0abff7055474263e165ef031a1ef10f4def66d0b
Abdul Samet Erkek <abdulsameterkek@gmail.com>
2022-01-19 21:15:32 +0300

    size is added

--- IntList.java
+++ IntList.java
@@ -7,9 +7,20 @@ public class IntList {
     rest = _intList;
   }
 
+  public int size(){
+    if(rest == null){
+      return 1;
+    }
+    return 1 + this.rest.size();
+  }
+
   public static void main(String[] args) {
+    //creating and adding 3 items to the intList
     IntList L = new IntList(5,null);
     L = new IntList(10,L);
     L = new IntList(15,L);
+
+    //print out the size
+    System.out.println(L.size());
   }
 }
